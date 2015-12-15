20f867fc917af3b03a1b35080c4922ee772b0e55
Emmanuel Peralta <eperalta@webgeoservices.com>
2015-12-15 14:35:59 +0100

    Adds Google maps api loader class.

---
+++ src/mapsloader.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const http = require('./http');
+/**
+ * @typedef {Object} MapsLoaderOptions
+ * @property {String} [version]
+ * @property {String} [channel]
+ * @property {String} [clientId]
+ * @property {Array.<String>} extraLibraries
+ */
+
+class MapsLoader {
+    /**
+     *
+     * @param {MapsLoaderOptions}[options]
+     */
+    constructor(options) {
+        /**
+         *
+         * @type {MapsLoaderOptions}
+         */
+        this.options = {
+            version: '3.22',
+            extraLibraries: ['places']
+        };
+
+        Object.assign(this.options, options);
+    }
+
+    /**
+     *
+     * @param {Function} callback
+     */
+    load(callback) {
+        http.getScript('https://www.google.com/jsapi').then(
+            () => {
+                google.load('maps', this.options.version, {
+                    callback: callback,
+                    other_params: 'libraries=' + this.options.extraLibraries.join(',')
+                })
+            }
+        );
+
+    }
+}
+
+module.exports = MapsLoader;
