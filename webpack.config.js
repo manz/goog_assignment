91e8eb2ab18c1ba66cfc2d8942caefb17e1c8e75
Emmanuel Peralta <eperalta@webgeoservices.com>
2015-12-16 15:55:15 +0100

    Adds babel loader to support Safari & Firefox.
    Safari and Firefox es6 support is not on par with Chrome.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
         path: __dirname,
         filename: 'bundle.js'
     },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
     resolve: {
         extensions: ['', '.js']
     },
